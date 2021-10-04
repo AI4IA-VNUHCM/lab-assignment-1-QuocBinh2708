7761dfe53adfb0dcdb2d2e68aa98e0e222f05d26
QuocBinh2708 <kamenrider27.sam@gmail.com>
2021-10-04 23:23:58 +0700

    update Exercise2

--- src/Exercise2.c
+++ src/Exercise2.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[]) {
 			min = testcase [b];
 		}
 	}
-	printf("Max:%d\n", max);
-	printf("Min:%d\n", min);
+	printf("Max: %d\n", max);
+	printf("Min: %d\n", min);
 	return 0;
 }
